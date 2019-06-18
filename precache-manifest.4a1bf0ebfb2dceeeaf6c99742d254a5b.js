dd2d0e49c7b84bd3c93e80001d67388c9e9c2f01
Pipas <19pipas@gmail.com>
2019-06-18 21:50:17 +0100

    Updates

---
+++ precache-manifest.4a1bf0ebfb2dceeeaf6c99742d254a5b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a17090a3a6ac9524b3a86d68758b06e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "03829bc1a63e36d55335",
+    "url": "/static/css/main.894e9240.chunk.css"
+  },
+  {
+    "revision": "2e778b3eb8b9bdbfc286",
+    "url": "/static/js/2.f39765c4.chunk.js"
+  },
+  {
+    "revision": "03829bc1a63e36d55335",
+    "url": "/static/js/main.97a2afa8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
