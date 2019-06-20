f70dbbf25935a9a9a4095df6dbd14d6e9978d20f
Pipas <19pipas@gmail.com>
2019-06-20 18:10:30 +0100

    Finish song search input

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,30 +1,18 @@
-import React, { useEffect, useState, useRef } from 'react'
+import React, { useEffect, useRef } from 'react'
 import { Spotify } from '../../utils/spoitfy'
 import './App.css'
-import SongInput from '../songInput/songInput';
-
-type onChange = (event: React.ChangeEvent<HTMLInputElement>) => void
+import SongInput from '../songInput/songInput'
 
 const App: React.FC = () => {
   const spotify = useRef(new Spotify())
 
   useEffect(() => {
-    //if (!spotify.current.isAuthenticated()) spotify.current.authenticateUser()
+    if (!spotify.current.isAuthenticated()) spotify.current.authenticateUser()
   }, [])
-
-  const onPlayClick = () => {
-    // if (query !== null && query !== '' && query.length > 3) {
-    //   spotify.current.searchTrack(query).then(data => {
-    //     if (data.tracks.items.length > 0) {
-    //       setImg(data.tracks.items[0].album.images[0].url)
-    //     }
-    //   })
-    // }
-  }
 
   return (
     <div className='app'>
-      <SongInput />
+      <SongInput spotify={spotify.current} />
     </div>
   )
 }
--- src/components/songInput/songInput.tsx
+++ src/components/songInput/songInput.tsx
@@ -2,46 +2,64 @@ import React, { useState, useEffect, useRef } from 'react'
 import './SongInput.css'
 import SearchLogo from '../../images/search.svg'
 import Result from './searchResult/Result'
+import { Spotify } from '../../utils/spoitfy'
 
 type onChange = (event: React.ChangeEvent<HTMLInputElement>) => void
 
-const SongInput: React.FC = () => {
+type SongInputProps = {
+  spotify: Spotify
+}
+
+const SongInput: React.FC<SongInputProps> = props => {
   const searchInput = useRef<HTMLInputElement>(null)
+  const [state, setState] = useState('')
   const [query, setQuery] = useState('')
-  const [isFocused, setIsFocused] = useState(false)
+  const [results, setResults] = useState<
+    { title: string; artist: string; cover: string }[]
+  >([])
 
   useEffect(() => {
-    if(searchInput.current) {
-      searchInput.current.addEventListener('focus', () => setIsFocused(true))
-      searchInput.current.addEventListener('focusout', () => setIsFocused(false))
+    if (searchInput.current) {
+      searchInput.current.addEventListener('focus', () => updateSize(true))
+      searchInput.current.addEventListener('focusout', () => updateSize(false))
     }
   })
+
+  const updateSize = (focused: boolean) => {
+    if (focused && query.length === 0) setState('focused')
+    else if (focused || query.length > 0) setState('focused')
+    else {
+      setResults([])
+      setState('')
+    }
+  }
+
+  const onSearchClick = () => {
+    if (query !== null && query !== '') {
+      props.spotify.searchTrack(query).then(data => {
+        if (data.tracks.items.length > 0) {
+          setResults(
+            data.tracks.items.slice(0, 3).map(item => ({
+              title: item.name,
+              artist: item.artists[0].name,
+              cover: item.album.images[0].url
+            }))
+          )
+        }
+      })
+    }
+  }
 
   const onQueryChange: onChange = e => {
     setQuery(e.target.value)
   }
 
-  const onPlayClick = () => {
-    // if (query !== null && query !== '' && query.length > 3) {
-    //   spotify.current.searchTrack(query).then(data => {
-    //     if (data.tracks.items.length > 0) {
-    //       setImg(data.tracks.items[0].album.images[0].url)
-    //     }
-    //   })
-    // }
-  }
-
-  const results = [
-    {
-      title: "Sgt. Pepper's Lonely Hearts Club Band",
-      artist: 'The Beatles',
-      cover:
-        'https://upload.wikimedia.org/wikipedia/en/5/50/Sgt._Pepper%27s_Lonely_Hearts_Club_Band.jpg'
-    }
-  ].map(track => <Result {...track} />)
+  const resultElements = results.map((result, i) => (
+    <Result key={i} {...result} />
+  ))
 
   return (
-    <div className='input'>
+    <div className={`input ${state}`} style={{height: `${88 + (64 * results.length) + (results.length ? 8 : 0)}px`}}>
       <div className='searchBar'>
         <input
           type='text'
@@ -50,12 +68,11 @@ const SongInput: React.FC = () => {
           onChange={onQueryChange}
           placeholder='Search'
         ></input>
-        <button onClick={onPlayClick}>
+        <button onClick={onSearchClick}>
           <img src={SearchLogo} alt=''></img>
         </button>
       </div>
-      {(isFocused || query.length > 0) && <p className="tooltip">Search for a spotify song to get started.</p>}
-      {(isFocused || query.length > 0) && <div className='results'>{results}</div>}
+      {!!results.length && <div className='results'>{resultElements}</div>}
     </div>
   )
 }
