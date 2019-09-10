8190d660b246e587ab9268bf7e63920ac8520127
Pipas <paulo.renato.a.correia@gmail.com>
2019-09-10 18:21:20 +0100

    Finish readme and add GitHub star button

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # Spotify Roulette
+
+Spotify Roulette is a browser application that allows you to play a simple game of Russian Roulette with spotify songs.
+
+You play here: [https://paulocorreia.me/spotifyroulette](https://paulocorreia.me/spotifyroulette).
+
+
+## Development
+
+To run and deploy locally run
+```
+yarn install
+```
+followed by
+```
+yarn start
+```
+
+To run tests simply run
+```
+yarn test
+```
+
+## Copyright and license
+
+Spotify Roulette is released under the MIT license. See [LICENSE](https://github.com/Pipas/spotifyroulette/blob/master/LICENSE) for details.
--- src/components/settings/Settings.tsx
+++ src/components/settings/Settings.tsx
@@ -14,32 +14,74 @@ type SettingsProps = {
 
 const WarningDialog: React.FC<SettingsProps> = props => {
   const onSelectBulletType = (option: number) => {
-    props.setSettings((settings: SettingOptions) => ({...settings, bulletType: option}))
+    props.setSettings((settings: SettingOptions) => ({
+      ...settings,
+      bulletType: option
+    }))
   }
-  
+
   const onToggleRandomBullet = () => {
-    props.setSettings((settings: SettingOptions) => ({...settings, randomBullet: !settings.randomBullet}))
+    props.setSettings((settings: SettingOptions) => ({
+      ...settings,
+      randomBullet: !settings.randomBullet
+    }))
   }
 
   const onToggleDangerMode = () => {
-    props.setSettings((settings: SettingOptions) => ({...settings, dangerMode: !settings.dangerMode}))
+    props.setSettings((settings: SettingOptions) => ({
+      ...settings,
+      dangerMode: !settings.dangerMode
+    }))
   }
 
   return (
-    <div className={`settingsContainer ${props.visible ? 'visible' : 'hidden'}`} onClick={props.toggleVisibility}>
+    <div
+      className={`settingsContainer ${props.visible ? 'visible' : 'hidden'}`}
+      onClick={props.toggleVisibility}
+    >
       <div className='settingsDialog' onClick={e => e.stopPropagation()}>
         <h2>Settings</h2>
         <h3>Bullet Type</h3>
-        <SettingChips selected={props.settings.bulletType} chips={['Songs', 'Albuns', 'Artists']} onSelect={onSelectBulletType}/>
+        <SettingChips
+          selected={props.settings.bulletType}
+          chips={['Songs', 'Albuns', 'Artists']}
+          onSelect={onSelectBulletType}
+        />
         <div className='row'>
-          <h3 className={props.settings.dangerMode ? 'disabled' : ''}>Random Bullet</h3>
-          <SettingToggle value={props.settings.randomBullet} onToggle={onToggleRandomBullet} disabled={props.settings.dangerMode} />
+          <h3 className={props.settings.dangerMode ? 'disabled' : ''}>
+            Random Bullet
+          </h3>
+          <SettingToggle
+            value={props.settings.randomBullet}
+            onToggle={onToggleRandomBullet}
+            disabled={props.settings.dangerMode}
+          />
         </div>
         <div className='row'>
           <h3>Danger Mode</h3>
-          <SettingToggle value={props.settings.dangerMode} onToggle={onToggleDangerMode} disabled={false}/>
+          <SettingToggle
+            value={props.settings.dangerMode}
+            onToggle={onToggleDangerMode}
+            disabled={false}
+          />
         </div>
-        <img className='close' src={close} onClick={props.toggleVisibility} alt=''></img>
+        <div className='github'>
+          <p>Source on GitHub</p>
+          <a
+          className='github-button'
+          href='https://github.com/pipas/spotifyroulette'
+          data-icon='octicon-star'
+          aria-label='Star pipas/spotifyroulette on GitHub'
+        >
+          Star
+        </a>
+        </div>
+        <img
+          className='close'
+          src={close}
+          onClick={props.toggleVisibility}
+          alt=''
+        ></img>
       </div>
     </div>
   )
